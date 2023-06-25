4d93a6a109dee0e00e0a51be4e09a73a44ba558d
JavierVN009 <118240208+JavierVN009@users.noreply.github.com>
2023-06-25 03:29:36 -0600

    Se sustituyó todo el código (se guardó copia del anterior)

--- js/cb.js
+++ js/cb.js
@@ -99,15 +99,19 @@ function crearTabla() {
   
   
   // Crear la tabla con la matriz resultado
-  function crearTablaResultado(matriz) {
+  function crearTablaResultado(matriz) 
+  {
     // Obtener la tabla y su cuerpo
     var tablaResultado = document.getElementById("tablaResultado");
     var cuerpoTablaResultado = tablaResultado.createTBody();
+
+    var f = matriz.length;
+    var k = matriz[0].length;
   
     // Crear cada fila y sus celdas
     for (var i = 0; i < matriz.length; i++) {
       var filaResultado = cuerpoTablaResultado.insertRow();
-      for (var j = 0; j < matriz[i].length; j++) {
+      for (var j = matriz.length; j < matriz[i].length; j++) {
         var celdaResultado = filaResultado.insertCell();
         var entradaResultado = document.createElement("input");
         entradaResultado.type = "text";
@@ -115,17 +119,109 @@ function crearTabla() {
   
         // Convertir el número a fracción
         var fraccion = decimalAFraccion(matriz[i][j][0], matriz[i][j][1]);
-  
+        
+        f++;
+        k++;
+
         entradaResultado.value = fraccion;
   
         celdaResultado.appendChild(entradaResultado);
       }
     }
   }
+
+  // Crear la tabla con la matriz resultado de la matriz A
+    function crearTablaResultadoA(matriz) 
+    {
+      // Obtener la tabla y su cuerpo
+      var tablaResultado = document.getElementById("tablaA");
+      var cuerpoTablaResultado = tablaResultado.createTBody();
   
+      var f = matriz.length;
+      var k = matriz[0].length;
+      // Crear cada fila y sus celdas
+      for (var i = 0; i < matriz.length; i++) 
+      {
+        var filaResultado = cuerpoTablaResultado.insertRow();
+        for (var j = 0; j < matriz[i].length; j++) 
+        {
+          var celdaResultado = filaResultado.insertCell();
+          var entradaResultado = document.createElement("input");
+          entradaResultado.type = "text";
+          entradaResultado.name = "resultado[" + i + "][" + j + "]";
+          
+          f++;
+          k++;
+  
+          entradaResultado.value = matriz[i][j];
+    
+          celdaResultado.appendChild(entradaResultado);
+        }
+      }
+    }
+
+  // Crear la tabla con la matriz resultado de la matrizB
+    function crearTablaResultadoB(matriz) 
+    {
+      // Obtener la tabla y su cuerpo
+      var tablaResultado = document.getElementById("tablaB");
+      var cuerpoTablaResultado = tablaResultado.createTBody();
+  
+      var f = matriz.length;
+      var k = matriz[0].length;
+      // Crear cada fila y sus celdas
+      for (var i = 0; i < matriz.length; i++) 
+      {
+        var filaResultado = cuerpoTablaResultado.insertRow();
+        for (var j = 0; j < matriz[i].length; j++) 
+        {
+          var celdaResultado = filaResultado.insertCell();
+          var entradaResultado = document.createElement("input");
+          entradaResultado.type = "text";
+          entradaResultado.name = "resultado[" + i + "][" + j + "]";
+          
+          f++;
+          k++;
+  
+          entradaResultado.value = matriz[i][j];
+    
+          celdaResultado.appendChild(entradaResultado);
+        }
+      }
+    }
+  
+    function crearTablaResultado2(matriz) 
+    {
+      // Obtener la tabla y su cuerpo
+      var tablaResultado = document.getElementById("tablaRes");
+      var cuerpoTablaResultado = tablaResultado.createTBody();
+
+     var f = matriz.length;
+     var k = matriz[0].length;
+  
+      // Crear cada fila y sus celdas
+      for (var i = 0; i < matriz.length; i++) {
+      var filaResultado = cuerpoTablaResultado.insertRow();
+      for (var j = matriz.length; j < matriz[i].length; j++) {
+        var celdaResultado = filaResultado.insertCell();
+        var entradaResultado = document.createElement("input");
+        entradaResultado.type = "text";
+        entradaResultado.name = "resultado[" + i + "][" + j + "]";
+          
+          f++;
+          k++;
+  
+          entradaResultado.value = matriz[i][j];
+    
+          celdaResultado.appendChild(entradaResultado);
+        }
+      }
+    }
+
   // Función para convertir un decimal a fracción
   
-  function decimalAFraccion(a, b) {
+  function decimalAFraccion(a, b) 
+  {
     var decimal = a / b;
     var partes = decimal.toString().split(".");
     var signo = Math.sign(decimal);
@@ -137,6 +233,7 @@ function crearTabla() {
       return numerador + "/" + denominador;
     }
   }
+
   // Función para calcular el máximo común divisor de dos números
   function gcd(a, b) {
     if (b === 0) {
@@ -162,7 +259,9 @@ function crearTabla() {
     
   }
   
-  // Calcular la matriz Gauss-Jordan
+  
+  // CALCULA EL CAMBIO DE BASE USANDO GAUSS-JORDAN
+
   function calcular() {
     // Validar la matriz
     if (!validarMatriz()) {
@@ -184,6 +283,8 @@ function crearTabla() {
   
     // Crear la matriz a partir de los inputs
     var matriz = crearMatriz();
+    var A = crearMatrizSec();
+    var B = crearMatrizSecB();
   
     var n = matriz.length;
     var m = matriz[0].length;
@@ -191,8 +292,28 @@ function crearTabla() {
     var i = 0;
     var z = 0;
     var p = [];
+
+    //Revisa que el determinante no se haga cero (sinó, no sería base)
+
+    var detA = determinanteRecursivo(A);
+    var detB = determinanteRecursivo(B);
+
+    //SI EL DETERMINANTE DE LA BASE A ES CERO
+    if(detA === 0)
+    {
+        alert("Tu matriz A no es Base");
+        return;
+    }
+
+    //SI EL DETERMINANTE DE LA BASE B ES CERO
+    if(detB === 0)
+    {
+        alert("Tu matriz B no es Base");
+        return;
+    }
+
   
-    // Resolver la matriz Gauss-Jordan
+    // Resolver la matriz Gauss-Jordan para el cambio de Base
     while ( j < m-1 ) {
       i = z;
       while ( i < n ) {
@@ -247,7 +368,7 @@ function crearTabla() {
       j += 1;
     }
   
-    // Crear la tabla con la matriz resultado
+    // Crear la tabla con la matriz resultado que nos indica el cambio de base.
     crearTablaResultado(matriz);
   }
   
@@ -298,6 +419,7 @@ function crearTabla() {
     document.getElementById("botonEliminarColumna").style.display = "none";
   }
   
+  //COMIENZA DESDE EL INICIO TODO EL MICROSITIO
   
   function borrarDatos() {
     window.location.href = "index.html";
@@ -405,49 +527,375 @@ function crearTabla() {
   });
   
   
-    // Crear la matriz que solo tiene las variables a operar, ommitiendo la columna de resultados.
-    function crearMatrizSec() 
+  // Crear la matriz que solo tiene las entradas a operar de la Base A, ommitiendo las columnas de la Base B.
+  function crearMatrizSec() 
+  {
+    var matriz = [];
+    var filas = document.getElementById("matriz").rows;
+    
+    for (var i = 0; i < filas.length; i++) 
     {
-      var matriz = [];
-      var filas = document.getElementById("matriz").rows;
-    
-      for (var i = 0; i < filas.length; i++) 
+      var celdas = filas[i].cells;
+      matriz[i] = [];
+      for (var j = 0; j < (celdas.length - filas.length); j++) 
       {
-        var celdas = filas[i].cells;
-        matriz[i] = [];
+        var valor = celdas[j].childNodes[0].value;
     
-        for (var j = 0; j < celdas.length - 1; j++) 
+        // Validar si se ingresó una letra
+        if (isNaN(parseFloat(valor))) 
         {
-          var valor = celdas[j].childNodes[0].value;
+          alert("MATRIZ INVÁLIDA: Se ha ingresado una letra. Por favor, verifique que se introdujeron solo números en las celdas.");
+          return null;
+        }
     
-          // Validar si se ingresó una letra
-          if (isNaN(parseFloat(valor))) 
+        // Convertir el valor a fracción si es necesario
+        if (valor.indexOf("/") >= 0) 
+        {
+          // Si es una fracción, convertirla a decimal y luego a fracción nuevamente
+          var partes = valor.split("/");
+          var numerador = parseFloat(partes[0]);
+          var denominador = parseFloat(partes[1]);
+          if (isNaN(parseFloat(denominador))) 
           {
             alert("MATRIZ INVÁLIDA: Se ha ingresado una letra. Por favor, verifique que se introdujeron solo números en las celdas.");
             return null;
           }
+          valor = [numerador, denominador];
+        } 
+        else 
+        {
+          valor = [parseFloat(valor), 1];
+        }
     
-          // Convertir el valor a fracción si es necesario
-          if (valor.indexOf("/") >= 0) 
+        matriz[i][j] = valor;
+      }
+    }
+    return matriz;
+  }
+
+
+
+    // Crear la matriz que solo tiene las entradas a operar de la Base B, ommitiendo las columnas de la Base A.
+
+  function crearMatrizSecB() 
+  {
+    var matriz = [];
+    var B = [];
+    var filas = document.getElementById("matriz").rows;
+    for (var i = 0; i < filas.length; i++) 
+    {
+      var celdas = filas[i].cells;
+      matriz[i] = [];
+      for (var j = filas.length; j < celdas.length; j++) 
+      {
+        var valor = celdas[j].childNodes[0].value;
+        // Validar si se ingresó una letra
+        if (isNaN(parseFloat(valor))) 
+        {
+          alert("MATRIZ INVÁLIDA: Se ha ingresado una letra. Por favor, verifique que se introdujeron solo números en las celdas.");
+          return null;
+        }
+    
+        // Convertir el valor a fracción si es necesario
+        if (valor.indexOf("/") >= 0) 
+        {
+          // Si es una fracción, convertirla a decimal y luego a fracción nuevamente
+          var partes = valor.split("/");
+          var numerador = parseFloat(partes[0]);
+          var denominador = parseFloat(partes[1]);
+          if (isNaN(parseFloat(denominador))) 
           {
-            // Si es una fracción, convertirla a decimal y luego a fracción nuevamente
-            var partes = valor.split("/");
-            var numerador = parseFloat(partes[0]);
-            var denominador = parseFloat(partes[1]);
-            if (isNaN(parseFloat(denominador))) 
-            {
-              alert("MATRIZ INVÁLIDA: Se ha ingresado una letra. Por favor, verifique que se introdujeron solo números en las celdas.");
-              return null;
-            }
-            valor = [numerador, denominador];
-          } 
-          else 
-          {
-            valor = [parseFloat(valor), 1];
+            alert("MATRIZ INVÁLIDA: Se ha ingresado una letra. Por favor, verifique que se introdujeron solo números en las celdas.");
+            return null;
           }
+          valor = [numerador, denominador];
+        } 
+        else 
+        {
+          valor = [parseFloat(valor), 1];
+        }
     
-          matriz[i][j] = valor;
+        matriz[i][j] = valor;
+      }
+    }
+    let m = 0;
+    for(i = 0; i < filas.length; i++)
+    {
+      B[i] = [];
+      for(j = filas.length; j < celdas.length; j++)
+      {
+        if(m === filas.length)
+        {
+          m = 0;
+        }
+        B[i][m] = matriz[i][j];
+        m++;
+      }
+    }
+    return B;
+  }
+
+//OBTIENE EL DETERMINANTE PARA QUE, EN CASO DE QUE SE HICIERA CERO, YA NI HACES NADA Y LE DICES AL USUARIO "NO ES BASE, NO LE MUEVA"
+
+function determinanteRecursivo(matriz) 
+{
+    var n = matriz.length;
+    // Caso base: matriz de 1x1
+    if (n === 1) 
+    {
+      return Math.round(matriz[0][0][0] / matriz[0][0][1]); // Devolver la fracción como resultado
+    }
+    
+    var det = 0;
+      
+    // Obtener el primer renglón de la matriz
+    var primerRenglon = matriz[0];
+      
+    for (var j = 0; j < n; j++) 
+    {
+        // Calcular el cofactor de la submatriz eliminando el primer renglón y la columna j
+        var submatriz = obtenerSubmatriz(matriz, 0, j);
+        var cofactor = primerRenglon[j][0] / primerRenglon[j][1] * determinanteRecursivo(submatriz);
+      
+        // Sumar o restar el cofactor al determinante dependiendo del índice de la columna
+        det += (j % 2 === 0) ? cofactor : -cofactor;
+    }
+      
+    return Math.round(det);
+}
+
+//CREA UNA MATRIZ AUXILIAR PARA OBTENER EL DETERMINANTE
+
+function obtenerSubmatriz(matriz, iEliminar, jEliminar) 
+{
+    var submatriz = [];
+    var n = matriz.length;
+  
+    for (var i = 1; i < n; i++) 
+    {
+      var fila = [];
+      for (var j = 0; j < n; j++) 
+      {
+        if (j !== jEliminar) 
+        {
+          fila.push(matriz[i][j]);
         }
       }
-      return matriz;
+      if (i !== iEliminar) 
+      {
+        submatriz.push(fila);
+      }
     }
+  
+    return submatriz;
+}
+
+//FUNCION QUE UTILIZA EL METODO DE GRAM SCHMIDT PARA OBTENER BASES ORTOGONALES jajaja 
+
+//GRAM SCHMIDT EN GENERAL
+
+function gramSchmidt(baseOriginal) 
+{
+  const n = baseOriginal.length;
+  var vector = [];
+
+  //CREAMOS UN NUEVO ARREGLO PARA NUESTROS VECTORES Y NO ANDAR BATALLANDO
+  for(let u = 0 ; u < n; u++)
+  {
+    vector[u] = [];
+    for(let v = 0; v < n; v++)
+    {
+      vector[u][v] = baseOriginal[v][u][0];
+    }
+  }
+
+  // CREAMOS UNA NUEVA MATRIZ PARA ALMACENAR LA BASE ORTOGONALIZADA
+  const baseOrtogonalizada = [];
+
+  // Iteramos sobre los vectores de la base original (O SEA, EL ARREGLO vector[n][m])
+  for (let i = 0; i < n; i++) {
+    // Inicializamos el vector ortogonal con una copia del vector de la base original
+    let vectorOrtogonal = vector[i].slice();
+
+    // Restamos las proyecciones de los vectores anteriores
+    for (let j = 0; j < i; j++) 
+    {
+      const productoInterno = interno(baseOrtogonalizada[j], vector[i]);
+      const magnitudCuadrada = interno(baseOrtogonalizada[j], baseOrtogonalizada[j]);
+      const factor = productoInterno / magnitudCuadrada;
+
+      // Restamos la proyección del vector ortogonal anterior
+      vectorOrtogonal = resta(vectorOrtogonal, escalar(factor, baseOrtogonalizada[j]));
+    }
+
+    // Agregamos el vector ortogonalizado a la base ortogonalizada
+    baseOrtogonalizada.push(vectorOrtogonal);
+  }
+
+  //PRUEBA PARA VOLVER A FRACCIONES
+  for(let i = 0 ; i < n ; i++)
+  {
+    for(let j = 0 ; j < n ; j++)
+    {
+      if(baseOrtogonalizada[i][j] === 0)
+      {
+        baseOrtogonalizada[i][j] = baseOrtogonalizada[i][j];
+      }
+      else if(baseOrtogonalizada[i][j] < 0)
+      {
+        baseOrtogonalizada[i][j] = baseOrtogonalizada[i][j] * (-1);
+        var v = decToFrac( baseOrtogonalizada[i][j] );
+        var fraccion = decimalAFraccion(v[0], v[1]);
+        baseOrtogonalizada[i][j] = fraccion;
+        baseOrtogonalizada[i][j] = baseOrtogonalizada[i][j] * -1;
+        if(isNaN(baseOrtogonalizada[i][j]))
+        {
+          baseOrtogonalizada[i][j] = '-' + fraccion;
+        }
+      }
+      else
+      {
+        console.log("aquí es normal");
+        var v = decToFrac( baseOrtogonalizada[i][j] );
+        var fraccion = decimalAFraccion(v[0], v[1]);
+        baseOrtogonalizada[i][j] = fraccion;
+        baseOrtogonalizada[i][j] = baseOrtogonalizada[i][j];
+      }
+      //baseOrtogonalizada[i][j] = fraccion;
+    }
+  }
+  //ACABA LA PRUEBA PARA VOLVER FRACCIONES
+
+  return baseOrtogonalizada;
+}
+
+//GRAM SCHMIDT PARA A
+function gramSchmidtA()
+{
+   // Validar la matriz
+   if (!validarMatriz()) 
+   {
+    return;
+   }
+  var tablaResultado = document.getElementById("tablaA");
+  while (tablaResultado.rows.length > 0) 
+  {
+    tablaResultado.deleteRow(0);
+  }
+  var inputsResultado = document.querySelectorAll("#tablaA input[type='text']");
+  inputsResultado.forEach(function(input) {
+    input.value = "";
+  });
+  let view = document.getElementsByClassName("infosec2");
+for (var i = 0; i < view.length; i++) 
+{
+  view[i].style.display = "flex";
+}
+
+
+//METE LA baseA EN OTRA MATRIZ PARA IMPRIMIRLA EN PANTALLA
+  var baseA = crearMatrizSec();
+
+  //PRIMERO OBTIENE LA MATRIZ QUE SALIÓ DE HACER GRAM SCHMIDT Y LA GUARDA EN A
+  var A = gramSchmidt(baseA);
+  let n = A.length;
+  crearTablaResultadoA(A);
+}
+
+//GRAM SCHMIDT PARA B
+
+function gramSchmidtB()
+{
+   // Validar la matriz
+   if (!validarMatriz()) 
+   {
+    return;
+   }
+  var tablaResultado = document.getElementById("tablaB");
+  while (tablaResultado.rows.length > 0) 
+  {
+    tablaResultado.deleteRow(0);
+  }
+  var inputsResultado = document.querySelectorAll("#tablaB input[type='text']");
+  inputsResultado.forEach(function(input) {
+    input.value = "";
+  });
+  let view = document.getElementsByClassName("infosec2");
+for (var i = 0; i < view.length; i++) 
+{
+  view[i].style.display = "flex";
+}
+
+
+//METE LA baseA EN OTRA MATRIZ PARA IMPRIMIRLA EN PANTALLA
+  var baseB = crearMatrizSecB();
+
+  //PRIMERO OBTIENE LA MATRIZ QUE SALIÓ DE HACER GRAM SCHMIDT Y LA GUARDA EN A
+  var B = gramSchmidt(baseB);
+  let n = B.length;
+  crearTablaResultadoB(B);
+}
+
+// Función auxiliar para calcular el producto interno entre dos vectores
+function interno(a, b) 
+{
+  const n = a.length;
+  let sum = 0;
+  for (let i = 0; i < n; i++) 
+  {
+    sum += a[i] * b[i];
+  }
+  return sum;
+}
+
+
+// Función auxiliar para restar dos vectores
+function resta(a, b) 
+{
+  const n = a.length;
+  const result = [];
+  for (let i = 0; i < n; i++) 
+  {
+    result.push(a[i] - b[i]);
+  }
+  return result;
+}
+
+
+// Función auxiliar para sumar dos vectores
+function suma(a, b)
+{
+  const n = a.length;
+  const result = [];
+  for (let i = 0; i < n; i++) 
+  {
+    result.push(a[i] + b[i]);
+  }
+  return result;
+}
+
+
+// Función auxiliar para multiplicar un vector por un escalar
+function escalar(scalar, vector) 
+{
+  const n = vector.length;
+  const result = [];
+  for (let i = 0; i < n; i++) 
+  {
+    result.push(scalar * vector[i]);
+  }
+  return result;
+}
+
+
+
+
+decToFrac = dec =>
+[...Array(1000).keys()].flatMap(
+  i => [...Array(1000).keys()].map(
+    j => [
+      i + 1, j + 1, (i + 1) / (j + 1),
+      Math.abs(((i + 1) / (j + 1)) - dec)
+    ]
+  )
+).sort((a, b) => a[3] - b[3])[0].slice(0, 2)
